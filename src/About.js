b1febd83a57cb45b702fb26b36589e98f83dd97f
Shane Vann-Shirley <ashanev@gmail.com>
2020-06-20 01:02:12 -0400

    Start work on about page.

--- src/About.js
+++ src/About.js
@@ -2,7 +2,24 @@ import React from "react";
 import "./about.css";
 
 function About(props) {
-  return <div className="about">Check back soon!</div>;
+  return (
+    <div className="about">
+      <div className="about-title">About Me</div>
+      <div className="about-header-sentence">
+        The journey to my becoming an UX Designer was definitely the road less
+        traveled and I loved every moment of it.
+      </div>
+      {/* <div className="contact-box-container">
+        <div className="contact-box">
+          <div className="contact-box-title">Let's Get in Touch!</div>
+          <div className="contact-box-message">
+            I’m always inspired to learn & share knowledge. Feel free to connect
+            with me with opportunities or just to say hi!
+          </div>
+        </div>
+      </div> */}
+    </div>
+  );
 }
 
 export default About;
