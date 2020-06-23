974d0ed1d2b8184b9bb9b335928bc6cc0a64d32c
Shane Vann-Shirley <ashanev@gmail.com>
2020-06-22 23:01:07 -0400

    Bubbles.

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "../css/app.css";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, withRouter } from "react-router-dom";
 import Home from "./Home";
 import Portfolio from "./Portfolio";
 import Header from "./Header";
@@ -8,6 +8,7 @@ import About from "./About";
 import ErrorPage from "./Error";
 import Footer from "./Footer";
 import { useLocation } from "react-router-dom";
+import { Particles } from "react-particles-js";
 
 function App() {
   const location = useLocation();
@@ -16,6 +17,70 @@ function App() {
 
   return (
     <div className="App">
+      <Particles
+        params={{
+          particles: {
+            number: {
+              value: 160,
+              density: {
+                enable: false,
+              },
+            },
+            color: {
+              value: "#b42d2f",
+            },
+            size: {
+              value: 3,
+              random: true,
+              anim: {
+                speed: 4,
+                size_min: 0.3,
+              },
+            },
+            line_linked: {
+              enable: false,
+            },
+            move: {
+              random: true,
+              speed: 1,
+              // direction: "top",
+              out_mode: "out",
+            },
+          },
+          interactivity: {
+            events: {
+              onhover: {
+                enable: true,
+                mode: "bubble",
+              },
+              onclick: {
+                enable: true,
+                mode: "repulse",
+              },
+            },
+            modes: {
+              bubble: {
+                distance: 250,
+                duration: 2,
+                size: 0,
+                opacity: 0,
+              },
+              repulse: {
+                distance: 400,
+                duration: 4,
+              },
+            },
+          },
+        }}
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          zIndex: -1,
+          background: "white",
+        }}
+      />
+
       <Header />
       <Switch>
         <Route path="/" component={Home} exact></Route>
